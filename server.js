ec77034bdf3a09c10dabce8b0cdbe48826e48a94
sajasmohammed <sajasam97@gmail.com>
2021-08-31 16:02:26 -0700

    mongo db connection configured

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const dotenv=require('dotenv');
 const morgan=require('morgan');
 const bodyparser=require('body-parser');
 const path = require('path');
+const connectDB=require('./server/database/connection')
 
 const app=express();
 
@@ -11,6 +12,9 @@ const PORT=process.env.PORT || 8080
 
 //log request
 app.use(morgan('tiny'));
+
+//mongodb connect
+connectDB();
 
 //pass request to body parser
 app.use(bodyparser.urlencoded({extended:true}))
