0e9d664d5fec3c3c82dfe99fc909030ea204e546
sajasmohammed <sajasam97@gmail.com>
2021-09-05 10:03:57 -0700

    rest api create and tested

---
+++ server/controller/controller.js
@@ -0,0 +1,79 @@
+const Userdb=require('../model/model');
+
+//create and save user
+exports.create=(req, res)=>{
+//validate request
+if(!req.body){
+    res.status(400).send({message:"Content ca't be empty"});
+    return;
+}
+//new user
+const user=new Userdb({
+    name:req.body.name,
+    email:req.body.email,
+    gender:req.body.gender,
+    status:req.body.status
+})
+
+//save user in database
+user
+    .save(user)
+    .then(data=>{
+        res.send(data);
+    })
+    .catch(err=>{
+        res.status(500).send({
+            message:err.message || "some error occured while creating"
+        });
+    });
+}
+
+//retrive and return all user/ retrive and return single user
+exports.find=(req, res)=>{
+Userdb.find()
+.then(user=>{
+    res.send(user)
+})
+.catch(err =>{
+    res.status(500).send({message: err.message || "Error Occured while retriving data"})
+})
+}
+
+//update user by id
+exports.update=(req, res)=>{
+if(!req.body){
+    return res
+    .status(400)
+    .send({message: "Update can't be empty"})
+}
+const id=req.params.id;
+
+Userdb.findByIdAndUpdate(id, req.body, {useFindAndModify:false})
+.then(data =>{
+    if(!data){
+        res.status(404).send({message: 'Canot be update user with ${id} my be user not found'});
+    }else{
+        res.send(data);
+    }
+})
+.catch(err=>{
+    res.status(500).send({message: err.message || "Error Update userinfo"})
+})
+}
+
+//delete user
+exports.delete=(req, res)=>{
+    const id=req.params.id;
+
+    Userdb.findByIdAndDelete(id)
+    .then(data=>{
+        if(!data){
+            res.status(404).send({message: "cannot be delete ${id} not found"});
+        }else{
+            res.send(data);
+        }
+    })
+    .catch(err=>{
+        res.status(500).send({message: err.message || "user id not found"});
+    })
+}
--- server/routes/router.js
+++ server/routes/router.js
@@ -1,9 +1,17 @@
 const express=require('express');
 const route=express.Router();
 const service=require('../services/render');
+const controller=require('../controller/controller');
 
+//route
 route.get('/', service.homeRoutes)
 route.get('/add_user', service.add_user)
 route.get('/update_user', service.update_user)
 
-module.exports=route
+//api
+route.post('/api/users', controller.create);
+route.get('/api/users', controller.find);
+route.put('/api/users/:id', controller.update);
+route.delete('/api/users/:id', controller.delete);
+
+module.exports=route;
